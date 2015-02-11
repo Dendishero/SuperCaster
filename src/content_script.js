e29cd5d2f17af420e509e9cb4abd9ff4f50f5764
Michael George <mgeorg21@gmail.com>
2015-02-11 17:11:05 -0600

    ADDING background.js file
    reorganizing the layout of plugin

--- src/content_script.js
+++ src/content_script.js
@@ -681,3 +681,19 @@ var iframeDocument = scriptsiniframe ;
 console.log('log '+iframeDocument.src);
 console.log(iframeDocument);
 }
+chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+    /* First, validate the message's structure */
+    if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+        /* Collect the necessary data 
+         * (For your specific requirements `document.querySelectorAll(...)`
+         *  should be equivalent to jquery's `$(...)`) */
+        var domInfo = {
+            total:   document.querySelectorAll('*').length,
+            inputs:  document.querySelectorAll('input').length,
+            buttons: document.querySelectorAll('button').length
+        };
+        /* Directly respond to the sender (popup), 
+         * through the specified callback */
+        response(domInfo);
+    }
+});
